eb049759e3e0a42cf74708c26e93d8c744aa0bb0
blanpa <pascalblansche@gmail.com>
2022-02-13 18:38:33 +0000

    fix

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import time
 from grove.grove_moisture_sensor import GroveMoistureSensor
-#import seeed_dht
 from grove.gpio import GPIO
 import sys
 import logging
 from datetime import datetime
+from grove.factory import Factory
 
 # connect to alalog pin 2(slot A2)
 sensor_moist = GroveMoistureSensor(2)
 
-# sensor_temp = seeed_dht.DHT("11", 12)
+sensor_temp = Factory.getTemper("NTC-ADC", 2)
 
 # Logging
 logging.basicConfig(filename='logs.log', filemode='w', level=logging.DEBUG) 
@@ -20,8 +20,8 @@ def main():
         # moist
         m = sensor_moist.moisture
 
-        #Temp and humi
-        # humi, temp = sensor_temp.read()
+        # Temp
+        temp = sensor_temp.temperature
 
         # Motion Sensor
         Motion_value = GPIO(5, GPIO.IN)
@@ -29,7 +29,7 @@ def main():
         # Logs
         now = datetime.now()
 
-        logs = f" Datetime: {now}, Moisture value: {m}, Humi value:{0}, Temp value: {0}, Motion_value: {Motion_value.read()}"
+        logs = f" Datetime: {now}, Moisture value: {m}, Temp value: {temp}, Motion_value: {Motion_value.read()}"
         logging.debug(logs)
         print(logs)
 
