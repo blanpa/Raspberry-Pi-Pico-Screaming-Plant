61fee21bbc36dc6a93710604703c8234360f8575
blanpa <pascalblansche@gmail.com>
2022-03-22 21:25:25 +0000

    code currenty works

--- app.py
+++ app.py
@@ -5,16 +5,32 @@ from grove.gpio import GPIO
 import sys
 import logging
 from datetime import datetime
-import playsound
+#import playsound
 #from grove.factory import Factory
 import psycopg2
+import os
+import sysconfig
 
+print(sysconfig.get_platform())
+
+try:
+    if sysconfig.get_platform() == "linux-armv7l":
+        path = os.path.join("/home", "pi", "Code", "Confidental", "psswd.txt")
+        PASSWORD = open(path, mode = "r").read()
+    else:
+        PASSWORD = open(os.path.join("..", "Confidental", "passwd.txt"), mode = "r").read()
+
+    print("Got the right Password")
+except:
+    print(path)
+    print("Password is not in the Confidental Directory!")
+    exit()
 
 # Database Connection
 conn = psycopg2.connect(
     dbname="plantdb", 
     user="pi", 
-    password="", 
+    password=PASSWORD, 
     host="localhost", port ="5432")
 
 conn.autocommit = True
@@ -44,33 +60,35 @@ def main():
         motion_value = motion.read()
         
         if motion_value == True:
-            if moist > 100:
-                playsound.playsound('test.mp3', False)
+            pass
+            # if moist > 100:
+            #     playsound.playsound('test.mp3', False)
 
-            if moist <= 100:
-                playsound.playsound('test.mp3', False)
+            # if moist <= 100:
+            #     playsound.playsound('test.mp3', False)
 
         # Value for Logs
         now = datetime.now()
 
-        logs = f" python_datetime: {now}, moisture_value: {moist}, temp_value: {temp}, motion_value: {Motion_value}"
+        logs = f" python_datetime: {now}, moisture_value: {moist}, temp_value: {temp}, motion_value: {motion_value}"
         logging.debug(logs)
         print(logs)
 
         # # Database plant_times_series_data
         cursor = conn.cursor()
         statement = f""" 
-        insert into people2 values(
-            'CURRENT_TIMESTAMP', 
-            '{m}',
-            '{temp}',
-            '{Motion_value.read()}');
+        INSERT INTO testtest1
+        VALUES(
+            CURRENT_TIMESTAMP,
+            {moist},
+            {temp},
+            {motion_value});
         """
         cursor.execute(statement)
         conn.commit()
-        conn.close()
+        #conn.close()
 
-        # Sleep for 1 Second, we dont want to get to much data, i hope
+        # Sleep for 1 Second, we dont want to get to much data
         time.sleep(1)
 
 if __name__ == '__main__':
--- create_psqldb.py
+++ create_psqldb.py
@@ -1,11 +1,26 @@
 import psycopg2
 
 # from sqlalchemy import create_engine
+print(sysconfig.get_platform())
+
+try:
+    if sysconfig.get_platform() == "linux-armv7l":
+        path = os.path.join("/home", "pi", "Code", "Confidental", "psswd.txt")
+        PASSWORD = open(path, mode = "r").read()
+    else:
+        PASSWORD = open(os.path.join("..", "Confidental", "passwd.txt"), mode = "r").read()
+
+    print("Got the right Password")
+except:
+    print(path)
+    print("Password is not in the Confidental Directory!")
+    exit()
+
 
 conn = psycopg2.connect(
     dbname="plantdb", 
     user="pi", 
-    password="", 
+    password=PASSWORD, 
     host="localhost", port ="5432")
 
 conn.autocommit = True
@@ -13,17 +28,16 @@ conn.autocommit = True
 cursor = conn.cursor()
 
 statement = """ 
-CREATE TABLE plant_times_series_data (
+CREATE TABLE testtest1 (
 	ts timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	uuid uuid NULL,
-	python_datetime int4 NULL,
 	moisture_value int4 NULL,
 	temp_value int4 NULL,
 	motion_value int4 NULL);
-
-SELECT create_hypertable('tbl_xyz', 'ts');
-
 """
+#SELECT create_hypertable('tbl_xyz', 'ts');
+
+# plant_times_series_data
+#	uuid uuid NULL,
 
 cursor.execute(statement)
   
