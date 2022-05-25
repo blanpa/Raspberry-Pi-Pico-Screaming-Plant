0503202cc528cfcbf0ee622d81e5ccb9dc1262ca
blanpa <pascal.blansche@gmail.com>
2022-05-25 17:43:49 +0200

    fix

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import time
+import time # to simulate a real time data, time loop
 
 # data
 import pandas as pd
@@ -8,11 +8,20 @@ import pandas as pd
 import sqlalchemy as sqla
 
 # Viz
-import plotly as py
+import time  # to simulate a real time data, time loop
+import numpy as np  # np mean, np random
+import pandas as pd  # read csv, df manipulation
+import plotly.express as px  # interactive charts
 
-st.header("Pascals real-time live Dashboard with IOT-Topfpflanze")
 
+st.set_page_config(
+    page_title="Real-Time Data Science Dashboard",
+    page_icon="✅",
+    layout="wide",
+)
 def main():
+    st.title("Pascals real-time live Dashboard with IOT-Topfpflanze")
+
 
     #conn = psycopg2.connect(
     #    database= st.secrets.db_credentials.database,
@@ -23,6 +32,13 @@ def main():
     
     engine_db = sqla.create_engine(st.secrets.db_credentials.database)
 
+
+
+    kpi1, kpi2, kpi3 = st.columns(3)
+
+    fig_col1, fig_col2 = st.columns(2)
+
+
     statement = """ 
     SELECT * FROM testtest2 ORDER BY ts DESC LIMIT 1000;
     """
@@ -31,14 +47,11 @@ def main():
         DF = pd.read_sql(Eintrag, con = engine_db)
 
 
-
-
-
         st.subheader("Detailed Data View")
         st.write(df)
         time.sleep(5)
 
 
 
-if name == __main__:
+if __name__ == "__main__":
     main()
